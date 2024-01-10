b957a04dae38a2c472f54b948cab586513fdfa61
vinhweb <vjhnguyen@gmail.com>
2024-01-10 12:26:18 +0700

    update bugs

--- src/database/migrations/2022_01_10_99999_create_chatify_messages_table.php
+++ src/database/migrations/2022_01_10_99999_create_chatify_messages_table.php
@@ -16,7 +16,7 @@ class CreateChatifyMessagesTable extends Migration
         Schema::create('ch_messages', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->bigInteger('from_id');
-            $table->bigInteger('to_id');
+	        $table->string('to_channel_id')->nullable();
             $table->string('body',5000)->nullable();
             $table->string('attachment')->nullable();
             $table->boolean('seen')->default(false);
